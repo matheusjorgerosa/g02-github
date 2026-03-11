928ffa753be28b16ce2724c9f8bb0656d5cbb113
JvWandermurem <joao.oliveira@sou.inteli.edu.br>
2026-03-11 14:54:50 -0300

    feat: adiciona alterações na main e go.mod para criação de usuário, automigrate do gorm e rota post

--- src/backend-web/cmd/api/main.go
+++ src/backend-web/cmd/api/main.go
@@ -1,25 +1,41 @@
 package main
 
 import (
+	"backend-web/internal/platform/database"
 	"backend-web/internal/platform/logger"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
+
+	"backend-web/internal/user"
+
+	
 )
 
 func main() {
 	logger.Init()
+	database.Init()
+
+	database.DB.AutoMigrate(&user.User{})
+	userService := &user.UserService{}
+	userHandler := user.NewUserHandler(userService)
+
+
 	defer logger.Sync()
     logger.Info("a aplicação está iniciando...", zap.String("env", "development"))
 	r := gin.New()
 
-r.GET("/health", func(c *gin.Context) {
+	r.GET("/health", func(c *gin.Context) {
 		logger.Info("Health check acedido", zap.String("ip", c.ClientIP()))
 		c.JSON(http.StatusOK, gin.H{"status": "up"})
 	})
+	r.POST("/admin/users", userHandler.CreateUser)
+
 
 	if err := r.Run(":8080"); err != nil {
 		logger.Fatal("Falha ao iniciar o servidor", err)
 	}
+
+	
 }
--- src/backend-web/go.mod
+++ src/backend-web/go.mod
@@ -5,6 +5,7 @@ go 1.25.0
 require (
 	github.com/gin-gonic/gin v1.12.0
 	go.uber.org/zap v1.27.1
+	golang.org/x/crypto v0.48.0
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.31.1
 )
@@ -41,7 +42,6 @@ require (
 	go.mongodb.org/mongo-driver/v2 v2.5.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/arch v0.25.0 // indirect
-	golang.org/x/crypto v0.48.0 // indirect
 	golang.org/x/net v0.51.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
 	golang.org/x/sys v0.42.0 // indirect
