7798f1a2dfed778191cdb153e369443b9af45ecb
JvWandermurem <joao.oliveira@sou.inteli.edu.br>
2026-03-12 08:10:44 -0300

    feat: adciona handler,shcemas login e service login com jwt

--- src/backend-web/internal/user/handler.go
+++ src/backend-web/internal/user/handler.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 	"github.com/gin-gonic/gin"
 	"backend-web/internal/platform/logger"
-	"go.uber.org/zap"
 	"strings"
 )
 
@@ -16,27 +15,80 @@ func NewUserHandler(s *UserService) *UserHandler {
 	return &UserHandler{service: s}
 }
 
+// CreateUser godoc
+// @Summary      Criar um novo usuário
+// @Description  Cria um usuário no sistema. Requer token de administrador.
+// @Tags         Admin
+// @Accept       json
+// @Produce      json
+// @Security     BearerAuth
+// @Param        request  body      CreateUserRequest  true  "Dados do novo usuário"
+// @Success      201      {object}  User
+// @Failure      403      {object}  map[string]string
+// @Router       /admin/users [post]
 func (h *UserHandler) CreateUser(c *gin.Context) {
 	var req CreateUserRequest
 
 	if err := c.ShouldBindJSON(&req); err != nil {
-		logger.Warn("Dados inválidos para criação de usuário", zap.Error(err))
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Verifique os dados enviados"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": translateValidationError(err)})
 		return
 	}
 
-	// Chama o Service 
 	user, err := h.service.CreateUser(req.Name, req.Email, req.Password, req.Role)
 	if err != nil {
-    // Se o erro contém "duplicate key", o erro é do cliente (409 Conflict ou 400 Bad Request)
-    if strings.Contains(err.Error(), "duplicate key") {
-        c.JSON(http.StatusConflict, gin.H{"error": "Este e-mail já está cadastrado"})
-        return
-    }
-    c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao salvar usuário"})
-    return
-}
-	// Retorna
+		if strings.Contains(err.Error(), "duplicate key") {
+			c.JSON(http.StatusConflict, gin.H{"error": "Este e-mail já está cadastrado"})
+			return
+		}
+		logger.Error("Erro ao criar usuário", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro interno ao processar cadastro"})
+		return
+	}
+
 	c.JSON(http.StatusCreated, user)
 }
 
+// Login godoc
+// @Summary      Realizar login
+// @Description  Autentica o usuário e retorna um token JWT
+// @Tags         Auth
+// @Accept       json
+// @Produce      json
+// @Param        request  body      LoginRequest  true  "Credenciais de login"
+// @Success      200      {object}  LoginResponse
+// @Failure      401      {object}  map[string]string
+// @Router       /login [post]
+func (h *UserHandler) Login(c *gin.Context) {
+	var req LoginRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Email ou senha em formato inválido"})
+		return
+	}
+
+	token, err := h.service.Login(req.Email, req.Password) // Note: Chame o método do service aqui
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, LoginResponse{Token: token})
+}
+
+func translateValidationError(err error) string {
+	errStr := err.Error()
+
+	if strings.Contains(errStr, "'Email' failed on the 'email'") {
+		return "O formato do e-mail é inválido."
+	}
+	if strings.Contains(errStr, "'Password' failed on the 'min'") {
+		return "A senha deve ter no mínimo 6 caracteres."
+	}
+	if strings.Contains(errStr, "failed on the 'required'") {
+		return "Todos os campos obrigatórios devem ser preenchidos."
+	}
+	if strings.Contains(errStr, "'Role' failed on the 'oneof'") {
+		return "O cargo (role) deve ser 'admin' ou 'user'."
+	}
+
+	return "Dados enviados estão incorretos. Verifique os campos."
+}
--- src/backend-web/internal/user/service.go
+++ src/backend-web/internal/user/service.go
@@ -1,8 +1,13 @@
 package user
 
 import (
+	"os"
+    "github.com/golang-jwt/jwt/v5"
+
 	"backend-web/internal/platform/database"
 	"golang.org/x/crypto/bcrypt"
+	"errors"
+	"time"
 )
 
 type UserService struct{}
@@ -29,4 +34,28 @@ func (s *UserService) CreateUser(name, email, password, role string) (*User, err
 	}
 
 	return &user, nil
+}
+
+
+func (s *UserService) Login(email, password string) (string, error) {
+	var user User
+	//Busca o usuário pelo e-mail
+	if err := database.DB.Where("email = ?", email).First(&user).Error; err != nil {
+		return "", errors.New("usuário não encontrado")
+	}
+
+	// Compara o hash da senha
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		return "", errors.New("senha inválida")
+	}
+
+	// Gera o Token JWT
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": user.ID,
+		"role":    user.Role,
+		"exp":     time.Now().Add(time.Hour * 24 *365).Unix(), // Expira em 24h
+	})
+
+	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
 }
