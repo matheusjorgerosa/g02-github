e7d63c5b34816fcd8ad0bcdbd6688fdd964081f7
matheusjorgerosa <matheus.rosa@sou.inteli.edu.br>
2026-03-14 22:03:53 -0300

    feat: altera vite config para receber imagem via cors

--- sp-density-map/vite.config.js
+++ sp-density-map/vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/login': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        bypass(req) {
+          if (req.method !== 'POST') return req.url;
+        },
+      },
+      '/admin': 'http://localhost:8080',
+      '/api': 'http://localhost:8080',
+      '/health': 'http://localhost:8080',
+    },
+  },
 })
